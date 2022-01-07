2c1de108758c332d03a24562a7a8d09c087d82d0
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-01-07 09:33:27 +0100

    first commit

---
+++ src/components/Emails.jsx
@@ -0,0 +1,15 @@
+import Email from "./Email"
+
+function Emails (props){
+    return( 
+        <main className="emails">
+        <ul>
+          {props.filteredEmails.map((email, index) => (
+            <Email email={email} index={index} toggleRead={props.toggleRead} toggleStar={props.toggleStar}/>
+          ))}
+        </ul>
+      </main>
+    
+)}
+
+export default Emails
---
+++ src/components/Title.jsx
@@ -0,0 +1,6 @@
+function Title (props){
+    return( 
+    <h1></h1>
+)}
+
+export default Title
